b9fe724d7e5282b60f4a5647d7109038a3dbbac2
Chris Lincoln <chris.lincoln12@gmail.com>
2019-05-22 11:48:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,8 +13,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  return true if text.scan(/\p{Punct}/).empty?
-  false
+  return true if text.scan(/\p{Punct}/)
 
 end
 
